23376d5f0dd899cfe62d7c93a4fe90dfc25cc542
Olotu Oluwagbemiga <>
2023-01-06 02:07:36 -0800

    Fixes t othe 2-args.py tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    if argv_count == 0:
-        print("{:d} arguments.".format(argv_count))
-    elif argv_count == 1:
-        print("{:d} argument:".format(argv_count))
-        print("{:d}: {:s}".format(index, sys.argv[1]))
-    else:
-        print("{:d} arguments:".format(argv_count))
-        while index <= argv_count:
-            print("{:d}: {:s}".format(index, sys.argv[index]))
-            index += 1
+import sys
+
+argv = sys.argv[1:]
+
+print(f"Number of argument(s): {len(argv)}", end="")
+if len(argv) == 1:
+    print(" argument", end="")
+else:
+    print("argument", end="")
+print(":", end="\n\n")
+
+for i in range(len(argv)):
+    print(f"{i+1}: {argv[i]}", end="\n")
